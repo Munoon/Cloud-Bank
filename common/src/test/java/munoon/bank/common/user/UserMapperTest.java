1d914d1e6668e58c7b5f31ae4b69fd5cf372ed96
Munoon <munoongg@gmail.com>
2020-12-04 12:53:58 +0200

    Fix UserMapperTest#asTo test

--- common/src/test/java/munoon/bank/common/user/UserMapperTest.java
+++ common/src/test/java/munoon/bank/common/user/UserMapperTest.java
@@ -11,7 +11,7 @@ class UserMapperTest {
     @Test
     void asTo() {
         User user = new User(100, "Nikita", "Ivchenko", "munoon", "password", "10", LocalDateTime.now(), Set.of(UserRoles.ROLE_ADMIN, UserRoles.ROLE_TEACHER));
-        UserTo expected = new UserTo(100, "Nikita", "Ivchenko", "munoon", "clazz", LocalDateTime.now(), Set.of(UserRoles.ROLE_ADMIN, UserRoles.ROLE_TEACHER));
+        UserTo expected = new UserTo(100, "Nikita", "Ivchenko", "munoon", "10", LocalDateTime.now(), Set.of(UserRoles.ROLE_ADMIN, UserRoles.ROLE_TEACHER));
         UserTo actual = UserMapper.INSTANCE.asTo(user);
         assertThat(actual).usingRecursiveComparison().ignoringFields("registered").isEqualTo(expected);
     }
