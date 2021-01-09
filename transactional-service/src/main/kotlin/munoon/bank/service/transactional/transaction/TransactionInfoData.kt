8eb9f71902cc1473a21edbd8cbc3cdd952d01fd5
Munoon <munoongg@gmail.com>
2021-01-09 14:00:03 +0200

    Make TransactionInfoData abstract class instead of interface

--- transactional-service/src/main/kotlin/munoon/bank/service/transactional/transaction/TransactionInfoData.kt
+++ transactional-service/src/main/kotlin/munoon/bank/service/transactional/transaction/TransactionInfoData.kt
@@ -3,39 +3,27 @@ package munoon.bank.service.transactional.transaction
 import munoon.bank.service.transactional.card.Card
 import munoon.bank.service.transactional.card.CardDataTo
 
-interface TransactionInfoData {
-    val transactionType: UserTransactionType
-}
+abstract class TransactionInfoData(val transactionType: UserTransactionType)
 
 data class FineAwardTransactionInfoData(
         val userId: Int,
         val fineAwardData: FineAwardDataTo
-) : TransactionInfoData {
-    override val transactionType = fineAwardData.type.originalType
-}
+) : TransactionInfoData(fineAwardData.type.originalType)
 
 data class BuyCardTransactionInfoData(
         val userId: Int,
         val cardPrice: Double,
         val cardDataTo: CardDataTo
-) : TransactionInfoData {
-    override val transactionType = UserTransactionType.CARD_BUY
-}
+) : TransactionInfoData(UserTransactionType.CARD_BUY)
 
-data class AddCardTransactionInfoData(val card: Card) : TransactionInfoData {
-    override val transactionType = UserTransactionType.CARD_BUY
-}
+data class AddCardTransactionInfoData(val card: Card) : TransactionInfoData(UserTransactionType.CARD_BUY)
 
 data class TranslateMoneyTransactionInfoData(
         val userId: Int,
         val translateMoneyDataTo: TranslateMoneyDataTo
-) : TransactionInfoData {
-    override val transactionType = UserTransactionType.TRANSLATE_MONEY
-}
+) : TransactionInfoData(UserTransactionType.TRANSLATE_MONEY)
 
 data class SalaryTransactionInfoData(
         val userId: Int,
         val count: Double
-) : TransactionInfoData {
-    override val transactionType = UserTransactionType.SALARY
-}
+) : TransactionInfoData(UserTransactionType.SALARY)
