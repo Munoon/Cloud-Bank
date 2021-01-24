55ba166f7c0e223cf7bebc74c511d0ab9425d161
Munoon <munoongg@gmail.com>
2021-01-24 22:52:11 +0200

    Make login page more responsive

--- client/frontend/src/page/login/index.tsx
+++ client/frontend/src/page/login/index.tsx
@@ -5,7 +5,7 @@ import LoginForm from './LoginForm';
 import store from './store';
 
 export const LoginPage = () => (
-    <div className='shadow-2xl w-1/4 min-w-min bg-white p-10 m-auto mt-10 login-panel self-center'>
+    <div className='shadow-2xl min-w-min bg-white p-10 m-auto mt-10 login-panel self-center w-5/6 md:w-1/2 max-w-sm'>
         <LoginHeader />
         <LoginForm />
     </div>
